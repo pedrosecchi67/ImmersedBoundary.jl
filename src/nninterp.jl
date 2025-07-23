1b43010507133c215827530fc4c964bdcbeea1df
Pedro de Almeida Secchi <pedrosecchimail@gmail.com>
2025-07-23 19:10:56 -0300

    Added clamping to nearest cell in coarse-to-fine interpolations

--- src/nninterp.jl
+++ src/nninterp.jl
@@ -33,11 +33,15 @@ module NNInterpolator
     Note that, if `first_index` is true, then `Xc, X` will also be expected to have shape `(npts, ndims)`.
 
     If `n_neighbors` is not given, it is set to `2 ^ ndims`.
+
+    If the weight of a single point is such that `abs(w - 1) < tolerance`, the interpolation is replaced
+    by a simple fetching of the point.
     """
     function Interpolator(Xc::AbstractMatrix, X::AbstractMatrix, 
         tree::Union{KDTree, Nothing} = nothing;
         linear::Bool = true,
         first_index::Bool = false,
+        tolerance::Float64 = 1e-3,
         n_neighbors::Int = 0)
 
         if first_index
@@ -94,11 +98,10 @@ module NNInterpolator
             end
         end
 
-        threshold = sqrt(eps(eltype(dists)))
-        is_same_point = @. abs(weights - 1.0) < threshold
+        is_same_point = @. weights >= 1.0 - tolerance
         # find if all other weigths are zero
         let is_near_zero = map(
-            c -> sum(abs.(c) .< threshold) == kneighs - 1,
+            c -> sum(abs.(c) .< tolerance) == kneighs - 1,
             eachcol(weights)
         )
             is_same_point .*= is_near_zero'
