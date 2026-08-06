0993899b30d2647a91a84cd13a32c83391ab6da3
pedrosecchi67 <45898359+pedrosecchi67@users.noreply.github.com>
2026-08-06 10:01:17 -0300

    Enriched RAE test

--- test/rae2822.jl
+++ test/rae2822.jl
@@ -21,8 +21,20 @@ begin
         verbose = true,
     )
 
+    X = zeros(Float32, length(dom), ndims(dom))
+    dom(X) do part, X
+        X .= part.centers;
+    end
+    CG = volume_integral(dom, X) ./ 2500.0f0
+    @show CG
+
+    ny = zeros(Float32, length(dom))
+    impose_bc!(dom, "wall", ny) do bdry, ny
+        bdry.normals[:, 2]
+    end
+
     coarse_doms, coarseners, prolongators = multigrid(dom)
     
-    export_vtk("rae2822", dom)
+    export_vtk("rae2822", dom; ny = ny,)
     export_vtk("rae2822_coarse", coarse_doms[end])
 end
